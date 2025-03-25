409d074906129f6667067a851e3c98dab998ebfc
adane010 <120035366+amanuel1167@users.noreply.github.com>
2025-03-24 19:42:29 -0500

    Working on fixing sims w/Ryan

--- simulations.py
+++ simulations.py
@@ -5,24 +5,28 @@ from solutionTable import solution_table
 
 
 def simulatation(trials, starting_balance):
-    total_bet = 0
     total_winnings = 0
     
     for _ in range(trials):
+        
         # Start Game
         game = Blackjack(starting_balance)
         
         balance = starting_balance
-        user_turn = True  # Track if it's the user's turn
-        betting_phase = True  # Player must place a bet before starting
 
         #Intialize game_bet
         game_bet = 0
         
+        # Each time this while loop runs represents 1 "night out"...
+        # In this case, there is a max amount of money to spend in one trip.
+        # Also, betting rules can be tweaked... See below.
+        
         while game_bet <= starting_balance:
             # Rule for betting
-            bet = random.randint(MIN_BET, min(MAX_BET, game.user_balance))  # Random valid bet
+            bet = random.randint(1, game.user_balance)  # Random valid bet
+            balance -= bet
 
+            #Note about ABOVE: We should probably add a minimum betting option in the Blackjack script.. - Amanuel
 
             game_bet += bet
         
@@ -34,9 +38,13 @@ def simulatation(trials, starting_balance):
             winning_result = game.check_winner()
             game.adjust_balance(bet, winning_result)
             
-            total_winnings += game.user_balance - starting_balance
+        
+        total_winnings += game.user_balance - starting_balance
+
     
-    ev = total_winnings / total_bet if total_bet > 0
+    total_bet = trials * starting_balance
+    ev = total_winnings / total_bet
+
     return ev
 
 
