3aaaee305de1861eae0753236697be7da4b47ef5
adane010 <120035366+amanuel1167@users.noreply.github.com>
2025-05-02 16:37:24 -0500

    Default setttings

--- settings.py
+++ settings.py
@@ -13,9 +13,9 @@ OTHER_PLAYERS_HIT_LIMIT = 17
 
 #Note: The order of play is determined by the order in this dictionary
 PLAYERS = {
-            #'player1': [],    # Player 1's hand
-            #'player2': [],    # Player 2's hand
-            #'player3': [],    # Player 3's hand
+            'player1': [],    # Player 1's hand
+            'player2': [],    # Player 2's hand
+            'player3': [],    # Player 3's hand
             #'player4': [],    # Player 4's hand
             #'player5': [],    # Player 5's hand
             #'player6': [],    # Player 6's hand
